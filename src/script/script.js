fd6cb9a3599d4e6313db599766027917ab348cfb
imisfp <imisfp@naver.com>
2024-11-01 18:44:10 +0900

    스와이퍼 추가

---
+++ src/script/script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+  effect: "fade", // 페이드 효과
+  fadeEffect: {
+    crossFade: true, // 슬라이드 간 크로스 페이드
+  },
+  autoplay: {
+    delay: 3000, // 슬라이드 전환 간격 (3초)
+    disableOnInteraction: false, // 사용자 상호작용 후에도 자동 재생 계속
+  },
+  speed: 3000,
+  loop: true, // 루프 설정으로 끝없이 전환
+});
